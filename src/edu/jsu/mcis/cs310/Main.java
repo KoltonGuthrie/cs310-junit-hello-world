3c8e009afe3822f92f39c4c6c3bc77899c3d54da
guthr <kguthrie2@stu.jsu.edu>
2023-01-24 12:01:14 -0600

    Completed Participation Assignment 2

--- src/edu/jsu/mcis/cs310/Main.java
+++ src/edu/jsu/mcis/cs310/Main.java
@@ -17,7 +17,11 @@ public class Main {
     }
     
     public String reverse(String message) {
-        return message;
+        StringBuilder str = new StringBuilder();
+        
+        str.append(message);
+        
+        return str.reverse().toString();
     }
     
 }
